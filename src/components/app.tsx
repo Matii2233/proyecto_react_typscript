172ee9d0391f9b05fd4d3e6b443c483960da2766
Matias Salinas <matisalinas1602@gmail.com>
2024-09-25 17:48:32 -0300

    commit 4 'app.tsx' rout refactored

---
+++ src/components/app.tsx
@@ -0,0 +1,19 @@
+import { AppProduct } from "./AppProduct/AppProduct"
+// import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
+// import { ComponentForm } from "./components/ComponentForm/ComponentForm"
+// import { ComponentUseEffect } from "./components/ComponentUseEffect/UseEffect"
+// import { MiPrimerComponente } from "./components/MiPrimerComponente/MiPrimerComponente"
+
+export const App = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '2vh' }}>
+      {
+    //   <MiPrimerComponente text={"Texto desde propiedades"} color="#059" fontSize={2} />
+    //   <ComponentCounter />
+    //   <ComponentUseEffect />
+    //   <ComponentForm />
+      <AppProduct />
+      }
+    </div>
+  )
+}
