99cd9d3db28bf13490b42f30c143c27d35492c16
Matias Salinas <matisalinas1602@gmail.com>
2024-09-25 15:34:00 -0300

    commit componentForm 1 and commit hooks 1

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,16 @@
-import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
-import { ComponentUseEffect } from "./components/ComponentUseEffect/UseEffect"
-import { MiPrimerComponente } from "./components/MiPrimerComponente/MiPrimerComponente"
+//import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
+import { ComponentForm } from "./components/ComponentForm/ComponentForm"
+// import { ComponentUseEffect } from "./components/ComponentUseEffect/UseEffect"
+// import { MiPrimerComponente } from "./components/MiPrimerComponente/MiPrimerComponente"
 
 export const App = () => {
     return (
         <div style={{display: 'flex', flexDirection: 'column', gap: '2vh'}}>
-            <MiPrimerComponente
-            text={"Texto desde propiedades"}
-            color="#059"
-            fontSize={2}
-            />
 
+            {/* <MiPrimerComponente text={"Texto desde propiedades"} color="#059" fontSize={2}/>
             <ComponentCounter/>
-
-            <ComponentUseEffect/>
+            <ComponentUseEffect/> */}
+            <ComponentForm/>
         </div>
     )
 }
---
+++ src/components/ComponentForm/ComponentForm.tsx
@@ -0,0 +1,36 @@
+import "./formStyle.css"
+import { useForm } from "../hooks/useForm"
+
+export const ComponentForm = () => {
+
+    const {values, handleChange, resetForm} = useForm({email: '', nombre: '', edad: 0})
+
+    const handleSubmitForm = () => {
+        console.log(values)
+    }
+
+    return (
+        <div>
+            <div className="container-form">
+                <div>
+                    <h2>FORMULARIO</h2>
+                </div>
+
+                <div className="container-inputs" >
+                    <input value={values.email} name="email" type="email" placeholder="example@example.com" onChange={handleChange} />
+
+                    <input value={values.nombre} name="nombre" type="text" placeholder="Nombre" onChange={handleChange} />
+
+                    <input value={values.edad} name="edad" type="number" placeholder="Edad" onChange={handleChange} />
+
+                </div>
+
+                <div>
+                    <button onClick={handleSubmitForm} >ENVIAR</button>
+                    <button onClick={resetForm} >RESTABLECER</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
